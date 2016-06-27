f516a9e781a4cf419827a9f230371c0ea896ee79
Daniel Fonseca <dfonseca@mpagg.local>
2016-06-27 19:20:50 -0300

    Adiciona redis keyspace notifications

--- RedisConnectionTest/RedisConnection-RedisBoost/Program.cs
+++ RedisConnectionTest/RedisConnection-RedisBoost/Program.cs
@@ -10,11 +10,12 @@ namespace RedisConnection_RedisBoost {
         private static void Main(string[] args) {
             // RedisBoost usa connectionstring para configurar acesso ao Redis
             var connectionString = ConfigurationManager.ConnectionStrings["Redis"].ConnectionString;
-            var client = RedisClient.ConnectAsync(connectionString).Result;
+            //var client = RedisClient.ConnectAsync(connectionString).Result;
 
-            //SimpleExample(connectionString);
+            SimpleExample(connectionString);
             //ClassExample(connectionString);
-            PubSubMessage(connectionString);
+            //PubSubMessage(connectionString);
+            //ListenToChannel(connectionString);
         }
 
         private static void SimpleExample(string connectionString) {
@@ -165,6 +166,42 @@ namespace RedisConnection_RedisBoost {
                 }
             }
         }
+
+        private static void ListenToChannel(string connectionString) {
+            using (var pool = RedisClient.CreateClientsPool()) {
+                IRedisClient redisClient;
+                var redisKey = "RedisBoostKeyClass";
+                var redisKeyDelete = "RedisBoostKeyDeleteClass";
+
+                var func = new Funcionario {
+                    IdFuncionario = Guid.NewGuid(),
+                    Name = "Charles",
+                    LastName = "Lomboni",
+                    Age = 28
+                };
+
+                // Cria o client
+                using (redisClient = pool.CreateClientAsync(connectionString).Result) {
+                    // Faz de forma assincrona o inserir no Redis
+                    using (var subscriber = redisClient.SubscribeAsync("channel").Result) {
+                        // Obtendo a primeira mensagem do canal, utilizando o subscriber2
+                        var channelMessage = subscriber.ReadMessageAsync(ChannelMessageType.Message | ChannelMessageType.PMessage).Result;
+                        //Loop para pegar as 10 mensagens no canal
+                        while (channelMessage.Value.As<Funcionario>() != null) {
+                            var r3 = channelMessage.Value.As<Funcionario>();
+                            Console.WriteLine("Valor: {0}", r3.Name);
+                            channelMessage = subscriber.ReadMessageAsync(ChannelMessageType.Message | ChannelMessageType.PMessage).Result;
+                        }
+                        var c = channelMessage.Channels[0];
+                        var r = channelMessage.Value.As<Funcionario>();
+                    }
+                }
+            }
+        }
+
+
+
+
     }
 
     public class Funcionario {
